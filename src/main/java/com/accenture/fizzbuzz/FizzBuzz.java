2d784d2dd96cd00abb787320655f53d7f7807051
jaume vidal <jaumevibu@gmail.com>
2023-03-28 14:06:34 +0200

    Pass executeReturnsFizzBuzzIfNumDivisibleBy3And5

--- src/main/java/com/accenture/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/accenture/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,7 @@ package com.accenture.fizzbuzz;
 
 public class FizzBuzz {
     String execute(Integer num){
+        if(num%3==0 && num%5==0)return "FizzBuzz";
         if(num%3==0)return "Fizz";
         if(num%5==0)return "Buzz";
         return "";
